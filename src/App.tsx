c495ecb94fc6cce4dfc4d10cd1d6ad2518b8513d
KaroCodes <git@karo.codes>
2023-05-25 16:46:14 +1000

    basic maze generation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import Maze from './maze/Maze'
+import { MazePresenter } from './maze/maze_presenter'
+
+const mazePresenter = new MazePresenter(1024, 8);
 
 function App() {
 
     return (
         <>
-            Maze
+            <Maze presenter={mazePresenter} />
         </>
     )
 }
